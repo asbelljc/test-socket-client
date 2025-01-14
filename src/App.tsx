96aae68b4353960ca86e12b031ff80d3e5c26660
Jonathan Asbell <asbelljc@gmail.com>
2025-01-14 14:48:20 -0700

    finished test draft

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    const ws = new WebSocket('wss://test-socket-app-1tf0.onrender.com');
+
+    ws.onopen = () => {
+      console.log('WebSocket connected');
+      ws.send('Hello from client!');
+    };
+
+    ws.onmessage = (event) => {
+      setList(JSON.parse(event.data).data);
+    };
+
+    return () => ws.close();
+  }, []);
+
+  return (
+    <>
+      <div>USER LIST</div>
+      <ul>
+        {list.map((ip) => (
+          <li key={ip}>{ip}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
